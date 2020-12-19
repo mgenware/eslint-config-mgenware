3468bc6f6596eddac32a2d08df1e11bb55bf68f4
Mgen <m@mgenware.com>
2020-12-19 15:50:10 +0800

    Remove fields that are not rules actually

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['airbnb-typescript-lite', 'plugin:@typescript-eslint/recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-plusplus': 'off',
