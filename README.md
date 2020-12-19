1997a1eefa74ce86e3009025948bdaf035282717
Mgen <m@mgenware.com>
2020-12-19 15:43:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# mingru-tsconfig
+
+A preset `tsconfig.json` for [mingru](https://github.com/mgenware/mingru) projects.
+
+## Usage
+
+Install mingru-tsconfig to your project:
+
+```sh
+# yarn
+yarn add mingru-tsconfig
+# or npm
+npm install mingru-tsconfig --save
+```
+
+Set `extends` to `mingru-tsconfig` in your local `tsconfig.json`:
+
+```json
+{
+  "extends": "mingru-tsconfig"
+}
+```
---
+++ main.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-typescript-lite', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-plusplus': 'off',
+    // Allow `for-of` loops.
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    // No need an extra line between one-line class members.
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    // Allow multiple classes per file.
+    'max-classes-per-file': 'off',
+    // Loosen destructuring rules.
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    // Handled by prettier.
+    '@typescript-eslint/indent': 'off',
+    // Handled by prettier.
+    'operator-linebreak': 'off',
+    // Handled by prettier.
+    'function-paren-newline': 'off',
+    // Handled by prettier.
+    'implicit-arrow-linebreak': 'off',
+    // Handled by prettier.
+    'object-curly-newline': 'off',
+    // No need explicit func return type.
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // Allow unused vars with "_" prefix.
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    // Enable switch exhaustiveness check.
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+  },
+};
