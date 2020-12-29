832fc826a40e430808ea6611c4900081a3b234f1
Mgen <m@mgenware.com>
2020-12-29 23:18:08 +0800

    Add a real test

---
+++ test/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', '../main.js'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+};
