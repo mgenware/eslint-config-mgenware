5dcedb71342aad180248dc85ef76235fc80abe2d
Mgen <m@mgenware.com>
2021-12-25 19:48:37 +0800

    Remove duplicated plugin:@typescript-eslint/recommended

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', '../main.js'],
+  extends: ['../main.js'],
   parserOptions: {
     project: './test/tsconfig.json',
   },
