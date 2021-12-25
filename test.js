0d42a1632d70268a3198e6dea030bea5a9387275
Mgen <m@mgenware.com>
2021-12-25 19:49:37 +0800

    Test against errors

---
+++ test.js
@@ -0,0 +1,40 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const files = ['success', 'ifFunc'];
+const errors = [null, '@typescript-eslint/no-unnecessary-condition'];
+
+function panic(msg) {
+  console.error(msg);
+  process.exit(1);
+}
+
+async function eslint(fileName, expectedErr) {
+  let err;
+  let out = '';
+  try {
+    const cmd = `eslint --max-warnings 0 -c ./test/.eslintrc.js test/${fileName}.ts`;
+    console.log(`>> ${cmd}`);
+    const { stdout } = await exec(cmd);
+    out = stdout;
+  } catch (e) {
+    err = e;
+    out = e.stdout;
+  }
+  if (expectedErr) {
+    if (!err) {
+      panic(`Expected error ${expectedErr}. Got success`);
+    }
+    if (!out.includes(expectedErr)) {
+      panic(`Expected error ${expectedErr}. Got ${out}`);
+    }
+  } else {
+    if (err) {
+      panic(`Expected success. Got error ${err?.message}`);
+    }
+  }
+}
+
+(async () => {
+  Promise.all(files.map((f, i) => eslint(f, errors[i])));
+})();
---
+++ test/ifFunc.ts
@@ -0,0 +1,5 @@
+function foo() {}
+
+if (foo) {
+  // foo
+}
